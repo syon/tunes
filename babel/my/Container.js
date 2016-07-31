a8d4d2599bed0d54621cbdaca807a52c9e461de2
syon <syon.xiii@gmail.com>
2016-07-31 13:22:31 +0900

    SoundPlayer fixed position

--- babel/my/Container.js
+++ babel/my/Container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Slider from 'material-ui/Slider';
 import Sound from 'react-sound';
 import SoundPlayer from './SoundPlayer';
 import TrackBox from './TrackBox';
@@ -42,21 +43,37 @@ class Container extends React.Component {
   }
 
   render() {
-    const style = {
-      paddingLeft: 256,
+    const styles = {
+      wrap: {
+        paddingLeft: 256,
+      },
+      fixed: {
+        position: 'fixed',
+        top: 0,
+        zIndex: 1,
+        width: '100%',
+        backgroundColor: 'rgb(72, 72, 72)',
+      },
+      trackbox: {
+        marginTop: 200,
+      },
     };
     return (
-      <div style={style}>
-        <AppBar title={this.state.track.title} />
-        <SoundPlayer
-          track={this.state.track}
-          status={this.state.status}
-        />
-        <TrackBox
-          setId={this.props.setId}
-          album={this.props.album}
-          select={this.handleSelect}
-        />
+      <div style={styles.wrap}>
+        <div style={styles.fixed}>
+          <AppBar title={this.state.track.title} />
+          <SoundPlayer
+            track={this.state.track}
+            status={this.state.status}
+          />
+        </div>
+        <div style={styles.trackbox}>
+          <TrackBox
+            setId={this.props.setId}
+            album={this.props.album}
+            select={this.handleSelect}
+          />
+        </div>
       </div>
     );
   }
