f3585facd21e2566247cfddf1452b8ff7d73acb7
syon <syon.xiii@gmail.com>
2016-08-20 15:46:19 +0900

    GoogleAnalytics Download Events

--- app/scripts/download.coffee
+++ app/scripts/download.coffee
@@ -1,4 +1,6 @@
 $ ->
   $(document).on 'click', '#download_btn', () ->
+    track_title = $('#track_id .title').text();
+    ga('send', 'event', 'DownloadPage', 'Download', track_title);
     track_id = $('#track_id').data 'track'
     Tunes.download(track_id)
--- public/assets/js/download.js
+++ public/assets/js/download.js
@@ -1,6 +1,8 @@
 $(function() {
   return $(document).on('click', '#download_btn', function() {
-    var track_id;
+    var track_id, track_title;
+    track_title = $('#track_id .title').text();
+    ga('send', 'event', 'DownloadPage', 'Download', track_title);
     track_id = $('#track_id').data('track');
     return Tunes.download(track_id);
   });
